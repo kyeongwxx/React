87b36d1f754f81445724a951afaa9b070a588e2b
kyeongwxx <kyeongwxx@gmail.com>
2022-06-24 21:52:43 +0900

    11.5 onToggle, onRemove 함수가 바뀌지 않게 하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,7 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { useState, useRef, useCallback } from 'react';
+import { useReducer, useRef, useCallback } from 'react';
 
 function createBulkTodos() {
   const array = [];
@@ -15,8 +15,23 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': // 새로 추가
+      return todos.concat(action.todo);
+    case 'REMOVE': // 새로 추가
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE': // 새로 추가
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+        );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기
@@ -29,29 +44,19 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1; // nextId에 1씩 더하기
-    },
-    [todos],
-  );
+    },[]);
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+      dispatch({ type: 'REMOVE', id });
+    },[]);
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+      dispatch({ type: 'TOGGLE', id });
+    },[]);
 
   return (
     <TodoTemplate>
